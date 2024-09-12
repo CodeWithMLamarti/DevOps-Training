58bc89d69751e53cd9e47898bedac43f82274f6b
CodeWithMLamarti <med.lmt012@gmail.com>
2024-09-12 16:56:01 +0100

    helloWorld Controller

---
+++ src/main/java/com/project/devopstraining/DevopsTrainingApplication.java
@@ -0,0 +1,15 @@
+package com.project.devopstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DevopsTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsTrainingApplication.class, args);
+    }
+
+
+}
